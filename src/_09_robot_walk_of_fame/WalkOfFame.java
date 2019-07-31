ac7702033390c7995ec4ec123dbcf6e978d350d0
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-07-31 14:28:31 -0700

    :smile:

--- src/_09_robot_walk_of_fame/WalkOfFame.java
+++ src/_09_robot_walk_of_fame/WalkOfFame.java
@@ -8,12 +8,26 @@ import org.jointheleague.graphical.robot.Robot;
 
 public class WalkOfFame {
 	public static void main(String[] args) {
+		
 		Robot rob = new Robot();
+		rob.miniaturize();
+		int X = 60;
 		// 1. Set the X position of the robot so that it starts on the left. 
 		// You also need to show the robot to see the result of this line.
-
+rob.moveTo(60, 300);
+rob.setSpeed(200);
 		// 2. Make the robot draw a star shape. Hint: 144.
+rob.penDown();
+for (int t = 0; t < 10; t++) {
 
+for (int i = 0; i < 5; i++) {
+rob.turn(144);
+rob.move(30);
+}
+
+rob.moveTo(X , 300);
+X = X+50;
+}
 		// 3. Set the size of the star to 30.
 
 		/** THE CHALLENGE: **/
