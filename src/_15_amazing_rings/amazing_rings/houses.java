6e8596312fcfa452561a9f6afd14ecedc67f5c87
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-09-11 16:17:09 -0700

    houses2

--- src/_15_amazing_rings/amazing_rings/houses.java
+++ src/_15_amazing_rings/amazing_rings/houses.java
@@ -1,6 +1,25 @@
 package _15_amazing_rings.amazing_rings;
 
+import org.jointheleague.graphical.robot.Robot;
 
 public class houses {
-
+public static void main(String[] args) {
+	int a=10;
+	Robot bob  = new Robot();
+	bob.miniaturize();
+	bob.setSpeed(100);
+	bob.setX(50);
+	bob.setY(500);
+	bob.penDown();
+	for(int i=0; i<10; i++) {
+		bob.move(100);
+		bob.turn(90);
+		bob.move(30);
+		bob.turn(90);
+		bob.move(100);
+		//bob.turn(180);
+		//bob.move(10);
+	}
+	
+}
 }
