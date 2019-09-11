70f361acff7df30b0866ed9a8b4b725a920a2f2f
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-09-11 16:58:09 -0700

    houses3

--- src/_15_amazing_rings/amazing_rings/houses.java
+++ src/_15_amazing_rings/amazing_rings/houses.java
@@ -1,25 +1,36 @@
 package _15_amazing_rings.amazing_rings;
 
+import java.awt.Color;
+import java.util.Random;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class houses {
 public static void main(String[] args) {
-	int a=10;
-	Robot bob  = new Robot();
+		Robot bob  = new Robot();
+		Robot.setWindowColor(Color.BLACK);
+
 	bob.miniaturize();
 	bob.setSpeed(100);
 	bob.setX(50);
 	bob.setY(500);
 	bob.penDown();
+
 	for(int i=0; i<10; i++) {
-		bob.move(100);
+		int r = new Random().nextInt(501-100)+100;
+		bob.setRandomPenColor();
+		bob.move(r);
 		bob.turn(90);
 		bob.move(30);
 		bob.turn(90);
-		bob.move(100);
-		//bob.turn(180);
-		//bob.move(10);
+		bob.move(r);
+		bob.turn(-90);
+		bob.setPenColor(Color.GREEN);
+		bob.move(30);
+		bob.turn(-90);
+		
 	}
+	
 	
 }
 }
